cc625e10090f7598d540ceaf92bedb34835bf259
M1nd111 <125198490+M1nd111@users.noreply.github.com>
2025-04-24 02:10:47 +1000

    common-library

---
+++ acc2-security-starter/src/main/java/com/example/starter/security/Acc2SecurityStarterApplication.java
@@ -0,0 +1,11 @@
+package com.example.starter.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Acc2SecurityStarterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Acc2SecurityStarterApplication.class, args);
+    }
+}
